bd7953e21452b41a2ecc910da6e7127aea1fab53
beluvigishreegagana <beluvigishreegagana@gmail.com>
2024-06-07 09:45:02 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
+variable "load_balancer_name" {
+  description = "The name prefix for all resources in the Load Balancer module."
+  type        = string
+}
 
+variable "resource_group_name" {
+  type = string
+}
+
+variable "resource_group_location" {
+  type = string
+}
